e37f2e2c8675c9eff95e296fd68a0b53a358257f
wei-key <2639223784@qq.com>
2023-12-20 21:25:58 +0800

    Initial commit

---
+++ src/main/java/com/weikey/liuguangapisdk/constant/HeaderConstant.java
@@ -0,0 +1,12 @@
+package com.weikey.liuguangapisdk.constant;
+
+public interface HeaderConstant {
+
+    /**
+     * 当响应头中有此字段时，表示响应数据为错误信息
+     */
+    String RESP_ERROR_NAME = "x-resp-error";
+
+    String RESP_ERROR_VALUE = "true";
+
+}
